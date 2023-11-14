6c568e926ff9059db4a031eb738713b80fac3192
s23483 <s23483@pjwstk.edu.pl>
2023-11-14 11:35:11 +0100

    fixed setting experiment

--- src/penguins/pipelines/modeling/nodes.py
+++ src/penguins/pipelines/modeling/nodes.py
@@ -8,16 +8,15 @@ from autogluon.tabular import TabularDataset, TabularPredictor
 import mlflow
  
 def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
-    train ,test = train_test_split(data,test_size=0.5)    
+    train ,test = train_test_split(data,test_size=0.2)    
     return test, test
  
  
 def train_model(train: pd.DataFrame, test: pd.DataFrame) -> TabularPredictor:
-    mlflow.create_experiment("penguins")
     mlflow.set_experiment("penguins")
     mlflow.autolog()
-    classificator = TabularPredictor(label="species")
-    classificator.fit(train)
+    classificator = TabularPredictor(label="species",log_to_file=False,problem_type="multiclass",eval_metric="accuracy")
+    classificator.fit(train, time_limit=120)
     y_pred = classificator.evaluate(test)
-    return classificator
+    return classificator.get_model_best()
     
