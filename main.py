68c619aa4289e2c09124f4277ccdcdb3902565fc
ciaciura <a.ciaciura@hotmail.com>
2024-01-16 13:56:55 +0100

    api

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from src.penguins.pipelines.serving.pipeline import create_pipeline
 from fastapi import FastAPI, HTTPException
 from kedro.io import DataCatalog, MemoryDataset
-from kedro.runner import SequentialRunner
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 
 
 @app.post("/run-pipeline/")
-def run_pipeline(species: str, island: str, bill_length_mm: float, bill_depth_mm: float, flipper_length_mm: float,
+def run_pipeline(island: str, bill_length_mm: float, bill_depth_mm: float, flipper_length_mm: float,
                  body_mass_g: float, sex: str):
     # Create the pipeline
     pipeline = create_pipeline()
@@ -15,7 +14,7 @@ def run_pipeline(species: str, island: str, bill_length_mm: float, bill_depth_mm
     # Set up the DataCatalog with the input value from the request
     io = DataCatalog({"api_data_catalog": MemoryDataset()})
     io.save("api_data_catalog",
-            f"species,island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex\n{species},{island},{bill_length_mm},{bill_depth_mm},{flipper_length_mm},{body_mass_g},{sex}")
+            f"island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex\n{island},{bill_length_mm},{bill_depth_mm},{flipper_length_mm},{body_mass_g},{sex}")
     # Run the pipeline and get the result
     result = SequentialRunner().run(pipeline, catalog=io).to_dict(orient="records")["predictions"]
     return {"result": result}
